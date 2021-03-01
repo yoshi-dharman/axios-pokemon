2734fdbaaba174b8808cedf24cc53d7ee4f8846d
yoshi-dharman <yoshi.dharman@ti.ukdw.ac.id>
2021-03-01 13:58:04 +0700

    add pikachu

--- src/App.js
+++ src/App.js
@@ -30,6 +30,11 @@ function App() {
               <div className="pokemon-data">
                 <p>Name : {pokemon.species.name}</p>
                 <p>Type : {pokemon.types[0].type.name}</p>
+                {pokemon.abilities.map((item,index) => {
+                    return (
+                      <p key={index} >Abilities {index +1} : {item.ability.name}</p>
+                    )
+                })}
               </div>
             </div>
           </div>
